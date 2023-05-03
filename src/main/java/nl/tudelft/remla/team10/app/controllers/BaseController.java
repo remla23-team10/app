243f83e482c97b4eaf5e56791ad21a6e62fad02e
Enrique Barba <35747594+enriquebarba97@users.noreply.github.com>
2023-05-03 16:30:21 +0200

    Initial commit with basic maven workflow

---
+++ src/main/java/nl/tudelft/remla/team10/app/controllers/BaseController.java
@@ -0,0 +1,19 @@
+package nl.tudelft.remla.team10.app.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import nl.tudelft.remla.team10.lib.util.VersionUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class BaseController {
+
+    @GetMapping("/")
+    public String index(){
+
+        log.info("LIBRARY VERSION: " + VersionUtil.getVersion());
+
+        return "index.html";
+    }
+}
---
+++ src/test/java/nl/tudelft/remla/team10/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.remla.team10.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
